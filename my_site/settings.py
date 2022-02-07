71a09f7eb80207657df02cbeb9100ecf354679c7
jiashi2@gmail.com <jiashi2@gmail.com>
2022-02-06 16:45:34 -0800

    commit on 2_6

--- my_site/settings.py
+++ my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&i_0z%83pa)jcc7q_meqzonr$yxhuyzf+eg8_aiuvl5n+@&+!4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
